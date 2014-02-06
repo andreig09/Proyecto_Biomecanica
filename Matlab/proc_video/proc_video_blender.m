642f65a44e8beeceb94639f53721e4131aa17385
gonzalito <gonzpk@gmail.com>
2014-02-06 20:33:34 -0200

    Version Actualizada del Tracking de Blender
    Version que obtienes los centroides de los puntos etiquetados
    
    (Vamos todos al baño de Pintado!)

--- Matlab/proc_video/proc_video_blender.m
+++ Matlab/proc_video/proc_video_blender.m
@@ -2,7 +2,11 @@ clc;
 clear all;
 close all;
 
-for i=1:200
+i_fin = 50;
+
+centroide_total = [];
+
+for i=1:i_fin
     if i <10
         imagen = sprintf('blender/test/image_00%d.tif',i);
     elseif i<100
@@ -13,7 +17,46 @@ for i=1:200
     
     I=imread(imagen);
     M=double(I(:,:,1));
-    imshow(uint8(M));
+    %figure(1)
+    %subplot(2,1,1)
+    %imshow(uint8(M));
+    N=M;
+    title(sprintf('Imagen Original %d',i));
+    %M = M-imopen(M,strel('disk',15));
+    figure(3)
+    %subplot(2,1,2)
+    %M_ = M;
+    se4 = strel('disk',2);
+    M_ = imdilate(M,se4);
     %imshow(uint8(255*(M>190)));
+    %imshow(uint8(homogen(M)));
+    %hy = fspecial('sobel');
+    %hx = hy';
+    %Iy = imfilter(double(M), hy, 'replicate');
+    %Ix = imfilter(double(M), hx, 'replicate');
+    %m = sqrt(Ix.^2 + Iy.^2);
+    %imshow(uint8());
+    M_=255*(M_>(0.75*255));
+    L = bwlabel(M_>0);[x,y]=find(L==2);
+    max(max(L))
+    m = [];
+    largo_m = 20;
+    punteado_m = 3;
+    for j=1:max(max(L))
+        centroide = regionprops(L==j,'Centroid');
+        m = [m;centroide.Centroid(1),centroide.Centroid(2)];
+        M_(round(centroide.Centroid(2)),round(centroide.Centroid(1))-largo_m:punteado_m:round(centroide.Centroid(1))+largo_m) = 150;
+        M_(round(centroide.Centroid(2))-largo_m:punteado_m:round(centroide.Centroid(2))+largo_m,round(centroide.Centroid(1))) = 150;
+    end
+    imshow(uint8(M_));
+    if i<10
+        imagen = sprintf('image/result/image_0%d.tiff',i);
+    else
+        imagen = sprintf('image/result/image_%d.tiff',i);
+    end
+    imwrite(M_,imagen);
+    %Lrgb = label2rgb(L, 'jet', 'w', 'shuffle');
+    %imshow(Lrgb)
+    title(sprintf('Imagen Procesada %d',i));
     pause(1/180)
 end;
