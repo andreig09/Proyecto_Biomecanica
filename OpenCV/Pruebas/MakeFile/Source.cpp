9048e66914802a673c392a43715c4c17dbf7796a
andreig09 <andreig09@gmail.com>
2014-03-18 00:36:41 -0300

    prueba makefile

---
+++ OpenCV/Pruebas/MakeFile/Source.cpp
@@ -0,0 +1,13 @@
+#include"ColorFilter.h"
+#include"detectarBlobs.h"
+
+int main(){
+	int x = 8;
+  int y = 9;
+
+  int resta = resta(y,x);
+  int suma = suma(y,x);
+
+  printf("la resta da: %d, y la suma da: %d \n",resta,suma);
+	
+}
