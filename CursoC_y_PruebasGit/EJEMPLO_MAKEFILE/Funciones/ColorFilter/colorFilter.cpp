2490a5cd96dfddf779f3a8cfa8fc3fd35e51f7cf
mgrrwu <mgrrwu@gmail.com>
2014-03-18 03:19:11 -0300

    Se agrega una carpeta dentro de CursoC_y_pruebasGit donde se tiene un makefile plenamente funcional, sirviendo de ejemplo

---
+++ CursoC_y_PruebasGit/EJEMPLO_MAKEFILE/Funciones/ColorFilter/colorFilter.cpp
@@ -0,0 +1,6 @@
+int resta(int a, int b){
+
+	int c = a - b;
+
+	return c;
+}
---
+++ CursoC_y_PruebasGit/EJEMPLO_MAKEFILE/Makefile_mas_simple/Source.cpp
@@ -0,0 +1,14 @@
+#include"colorFilter.h"
+#include"detectarBlobs.h"
+#include <stdio.h>
+
+int main(){
+	int x = 8;
+  int y = 9;
+
+  int res = resta(y,x);
+  int sum = suma(y,x);
+
+  printf("la resta da: %d, y la suma da: %d \n",res,sum);
+	
+}
