7cfaeece6a83f2feaa63cad144cf6773c4c137fa
andreig09 <andreig09@gmail.com>
2014-04-17 01:44:49 -0300

    motion capture

---
+++ motion_capture/include/funcionesAuxiliares.h
@@ -0,0 +1,9 @@
+#include <string>
+#include"cvblob.h"
+
+using namespace cv;
+using namespace cvb;
+
+std::string itoa(int n);
+double Distance2(double dX0, double dY0, double dX1, double dY1);
+void numerar(IplImage *img, CvBlobs blobs );
