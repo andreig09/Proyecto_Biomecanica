3d056c7c08e7682a55066e9d0c3af4ccd25280c2
andreig09 <andreig09@gmail.com>
2014-05-06 21:55:50 -0300

    anda el histograma

--- hist_ecual/Source.cpp
+++ hist_ecual/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include<cv.h>
  
 using std::cout;
 using std::cin;
@@ -19,9 +20,9 @@ void imhist(Mat image, int histogram[])
  
     // calculate the no of pixels for each intensity values
     for(int y = 0; y < image.rows; y++)
-        for(int x = 0; x < image.cols; x++)
+        for(int x = 0; x < image.cols; x++){
             histogram[(int)image.at<uchar>(y,x)]++;
- 
+		}
 }
  
 void cumhist(int histogram[], int cumhistogram[])
@@ -42,19 +43,21 @@ void histDisplay(int histogram[], const char* name)
         hist[i]=histogram[i];
     }
     // draw the histograms
-    int hist_w = 512; int hist_h = 400;
+    int hist_w = 1024; int hist_h = 800;
     int bin_w = cvRound((double) hist_w/256);
  
-    Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(255, 255, 255));
+	Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(255, 255, 255));
  
     // find the maximum intensity element from histogram
     int max = hist[0];
+	int maxi = 0;
     for(int i = 1; i < 256; i++){
         if(max < hist[i]){
             max = hist[i];
+			maxi = i;
         }
     }
- 
+	 
     // normalize the histogram between 0 and histImage.rows
  
     for(int i = 0; i < 256; i++){
@@ -67,7 +70,7 @@ void histDisplay(int histogram[], const char* name)
     {
         line(histImage, Point(bin_w*(i), hist_h),
                               Point(bin_w*(i), hist_h - hist[i]),
-             Scalar(0,0,0), 1, 8, 0);
+             Scalar(0,0,0), 3, 8, 0);
     }
  
     // display histogram
@@ -80,59 +83,60 @@ void histDisplay(int histogram[], const char* name)
 int main()
 {
     // Load the image
-    Mat image = imread("scene.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat image = imread("peladoverde.png", CV_LOAD_IMAGE_GRAYSCALE);
  
     // Generate the histogram
     int histogram[256];
     imhist(image, histogram);
  
     // Caluculate the size of image
-    int size = image.rows * image.cols;
-    float alpha = 255.0/size;
+    //int size = image.rows * image.cols;
+    //float alpha = 255.0/size;
  
     // Calculate the probability of each intensity
-    float PrRk[256];
-    for(int i = 0; i < 256; i++)
-    {
-        PrRk[i] = (double)histogram[i] / size;
-    }
+    //float PrRk[256];
+    //for(int i = 0; i < 256; i++)
+    //{
+      //  PrRk[i] = (double)histogram[i] / size;
+    //}
  
     // Generate cumulative frequency histogram
-    int cumhistogram[256];
-    cumhist(histogram,cumhistogram );
+    //int cumhistogram[256];
+    //cumhist(histogram,cumhistogram );
  
     // Scale the histogram
-    int Sk[256];
-    for(int i = 0; i < 256; i++)
-    {
-        Sk[i] = cvRound((double)cumhistogram[i] * alpha);
-    }
+    //int Sk[256];
+    //for(int i = 0; i < 256; i++)
+    //{
+     // Sk[i] = cvRound((double)cumhistogram[i] * alpha);
+    //}
  
  
     // Generate the equlized histogram
-    float PsSk[256];
-    for(int i = 0; i < 256; i++)
-    {
-        PsSk[i] = 0;
-    }
+    //float PsSk[256];
+    //for(int i = 0; i < 256; i++)
+    //{
+      //  PsSk[i] = 0;
+    //}
  
-    for(int i = 0; i < 256; i++)
-    {
-        PsSk[Sk[i]] += PrRk[i];
-    }
+    //for(int i = 0; i < 256; i++)
+    //{
+     //  PsSk[Sk[i]] += PrRk[i];
+    //}
  
-    int final[256];
-    for(int i = 0; i < 256; i++)
-        final[i] = cvRound(PsSk[i]*255);
- 
+    //int final[256];
+    //for(int i = 0; i < 256; i++){
+      // final[i] = cvRound(PsSk[i]*255);
+	//}
  
     // Generate the equlized image
-    Mat new_image = image.clone();
+    //Mat new_image = image.clone();
  
-    for(int y = 0; y < image.rows; y++)
-        for(int x = 0; x < image.cols; x++)
-            new_image.at<uchar>(y,x) = saturate_cast<uchar>(Sk[image.at<uchar>(y,x)]);
- 
+    //for(int y = 0; y < image.rows; y++)
+      //  for(int x = 0; x < image.cols; x++){
+        //    new_image.at<uchar>(y,x) = saturate_cast<uchar>(Sk[image.at<uchar>(y,x)]);
+		//}
+
    // Display the original Image
     namedWindow("Original Image");
     imshow("Original Image", image);
@@ -141,11 +145,11 @@ int main()
     histDisplay(histogram, "Original Histogram");
  
     // Display equilized image
-    namedWindow("Equilized Image");
-    imshow("Equilized Image",new_image);
+    //namedWindow("Equilized Image");
+    //imshow("Equilized Image",new_image);
  
     // Display the equilzed histogram
-    histDisplay(final, "Equilized Histogram");
+    //histDisplay(final, "Equilized Histogram");
  
     waitKey();
     return 0;
