6675bcede08b9d44484764c7b2c80029ff6915aa
andreig09 <andreig09@gmail.com>
2014-06-08 20:39:02 -0300

    limpieza de código

--- segmentacion/client/Source.cpp
+++ segmentacion/client/Source.cpp
@@ -12,14 +12,16 @@ using namespace cv;
 //using namespace cvb;
 using namespace std;
 
+CvCapture* capture =0;
+IplImage* frame=0;
+int frameNum = 0;
+double thresh;
+double thresh2;
+
 int main(int argc, char *argv[]){
 	
 	//Obtener video y separarlo en cuadros
 	///////////////////////////////////////////////////////////////////////////////////////////////////
-	CvCapture* capture =0;
-	IplImage* frame=0;
-	int frameNum = 0;
-
 	if ( argc != 2 ) {// argc should be 2 for correct execution
     // We print argv[0] assuming it is the program name
     cout<<"Cantidad de argumentos incorrecta";
@@ -42,30 +44,28 @@ int main(int argc, char *argv[]){
     cvNamedWindow("Video");      
     
 	//Tamaño del frame y frecuencia:  
-    double dWidth = cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-    double dHeight = cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-    int fps = cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
-	cout << "Frame Size = " << dWidth << "x" << dHeight << endl;
-    cout << "FPS = " << fps << endl;
-	Size frameSize(static_cast<int>(dWidth), static_cast<int>(dHeight));
+    //double dWidth = cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+    //double dHeight = cvGetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+    //int fps = cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+	//cout << "Frame Size = " << dWidth << "x" << dHeight << endl;
+    //cout << "FPS = " << fps << endl;
+	//Size frameSize(static_cast<int>(dWidth), static_cast<int>(dHeight));
 
 	//salidas de video
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    VideoWriter oVideoWriter ("filtro.avi", CV_FOURCC('M','P','4','2'), fps, frameSize, false); //initialize the VideoWriter object 
+    //VideoWriter oVideoWriter ("filtro.avi", CV_FOURCC('M','P','4','2'), fps, frameSize, false); //initialize the VideoWriter object 
     //CV_FOURCC('P','I','M','1')
-    if ( !oVideoWriter.isOpened() ) //if not initialize the VideoWriter successfully, exit the program
-    {
-       cout << "ERROR: Failed to write the video" << endl;
-       return -1;
-    }
+    //if ( !oVideoWriter.isOpened() ) //if not initialize the VideoWriter successfully, exit the program
+    //{
+    //   cout << "ERROR: Failed to write the video" << endl;
+    //   return -1;
+    //}
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
    
 	//VALORES INICIALES:
 
 	//Determinar umbral
-	double thresh;
-	double thresh2;
 	thresh = callOtsuN(frame);
 	thresh2 = thresh*255;
 	//cout << "max threshold: " << thresh2 << "\n" ;
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]){
 
 		   XMLAddFrame(frameNum,detblobs.blobs); //Agregar los blobs de este frame en el xml
 		   
-		   oVideoWriter.write(imgThresh); //writer the frame with blobs detected
+		   //oVideoWriter.write(imgThresh); //writer the frame with blobs detected
 		   			 
 		   //Mostrar video original		   
 		   cvShowImage("Video", frame);
