93f1910a274b4b9fb5edc7f679bcb0b9021a75ab
andreig09 <andreig09@gmail.com>
2014-08-16 21:57:08 -0300

    cambio id xmls

--- segmentacion/lib/funcionesAuxiliares.cpp
+++ segmentacion/lib/funcionesAuxiliares.cpp
@@ -82,11 +82,14 @@ void startXML(){
 
 //Funcion para escribir el xml de los markers detectados
 void XMLAddBlobs(CvBlobs blobs, FILE *file){
+	int i =0;
 	CvPoint centroide;
 	for (CvBlobs::const_iterator it=blobs.begin(); it!=blobs.end(); ++it){
+		i++;
 		centroide.x = it->second->centroid.x;
 		centroide.y = it->second->centroid.y;
-		fprintf(file, "\t\t%s%i%s\n", "<Marker id=\"", it->first, "\" >");
+		//fprintf(file, "\t\t%s%i%s\n", "<Marker id=\"", it->first, "\" >");
+		fprintf(file, "\t\t%s%i%s\n", "<Marker id=\"", i, "\" >");
 		fprintf(file, "\t\t\t%s%i%s%i%s\n", "<Centroid x=\"", centroide.x, "\" y=\"", centroide.y, "\" />");
 		fprintf(file, "\t\t%s\n", "</Marker>");
 	}
