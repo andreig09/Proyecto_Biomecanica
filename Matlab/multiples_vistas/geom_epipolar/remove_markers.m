3070ef21d6a4601e3ab7cc611a4f50471a51f6c2
mgrrwu <mgrrwu@gmail.com>
2014-08-23 22:35:21 -0300

    Especulaciones y demás cambios en la estructura

--- Matlab/multiples_vistas/geom_epipolar/remove_markers.m
+++ Matlab/multiples_vistas/geom_epipolar/remove_markers.m
@@ -1,34 +1,43 @@
-function structure = remove_markers(structure, n_frame, names)
-%Funcion que remueve los marcadores de la estructura structure cuyos nombres se encuentran en el string names
-%para un cierto frame
+function structure = remove_markers(structure, frame, name)
+%Funcion que remueve los marcadores cuyos nombres se encuentran en el string names de la estructura structure
+%en los frames indicados en el vector frame
 
 %% CUERPO DE LA FUNCION
 
-%Obtengo los indices de los marcadores que poseen los nombres en 'names'
-n_names=length(names);
-index_x = cell(1, length(names)); %de esta manera puedo prealocate la variable
-for k=1:n_names %hacer con cada elemento de names
-    index_x{k} = find_name(structure, n_frame, names(k));
-end
-index_x = cell2mat(index_x); %obtengo una matriz ordenada cuya primer fila son indices y la segunda indica nro camara
 
- 
-index_x = index_x(1,:); %me quedo solo con los indices
-n_index = length(index_x);%numero de indices en index_x
+n_names=length(name);
+n_frames = length(frame);
+
+%Obtengo los indices de los marcadores que poseen los nombres en 'name' a lo largo de los frames en la variable 'frame'
+index_x = cell(1, length(name)); %de esta manera puedo prealocate la variable
+for k=1:n_names %hacer con cada elemento de names
+    index_x{k} = find_marker_by_name(structure, name(k), 'frame', frame); %busco en los frames indicados en el vector frame, el nombre name(k) 
+    %index_x{k} es una matriz de dos filas, la primera indica el indice encontrado y la segunda el frame donde se encontro
+end
+index_x = cell2mat(index_x); %obtengo una matriz ordenada cuya primer fila son indices y la segunda indica nro de frame asociado
+
+
+n_index = size(index_x, 2);%numero de indices de index_x 
 str_blank=cell(1, n_index);
 for i=1:n_index %relleno el cell con espacios en blanco
     str_blank{i}=' ';
 end
-%borro lo referente a la estructura marker
-structure = set_info(structure, 'frame', n_frame, 'marker', index_x, 'coord', zeros(3,n_index) ); %setea con las columnas de "info17" las coordenadas de todos los marcadores en el frame 1 de structure
-structure = set_info(structure, 'frame', n_frame, 'marker', index_x, 'name', str_blank); %setea con las columnas del cell string "info18" los nombres de todos los marcadores en el frame 1 de structure
-structure = set_info(structure, 'frame', n_frame, 'marker', index_x, 'state', zeros(1,n_index) ); %setea con las columnas de "info19" un vector con los estados de  todos los marcadores en el frame 1 de structure
-structure = set_info(structure, 'frame', n_frame, 'marker', index_x, 'source_cam', zeros(1,n_index) );  %setea con las columnas de "info20" un vector con las camaras fuente de todos los marcadores en el frame 1 de structure
-%borro lo referente a la estructura frame
-n_markers=get_info(structure, 'frame', n_frame, 'n_markers');%obtengo el numero de marcadores en el frame
-n_markers = n_markers - n_index; %actualizo el numero de marcadores en el frame
-structure = set_info(structure, 'frame', n_frame, 'n_markers', n_markers); 
-
+ 
+for k = 1:n_frames %hacer para cada frame
+    %encuentro los indices de index_x que pertenecen al frame k
+    colum = (index_x(2, :)==k);
+    %borro lo referente a la estructura marker
+    structure = set_info(structure, 'frame', frame(k), 'marker', index_x(1,colum), 'coord', zeros(3,n_index) ); %setea con las columnas, las coordenadas de todos los marcadores en el frame 1 de structure
+    structure = set_info(structure, 'frame', frame(k), 'marker', index_x(1,colum), 'name', str_blank); %setea con las columnas del cell string los nombres de todos los marcadores en el frame 1 de structure
+    structure = set_info(structure, 'frame', frame(k), 'marker', index_x(1,colum), 'state', zeros(1,n_index) ); %setea con las columnas,  un vector con los estados de  todos los marcadores en el frame 1 de structure
+    structure = set_info(structure, 'frame', frame(k), 'marker', index_x(1,colum), 'source_cam', zeros(1,n_index) );  %setea con las columnas, un vector con las camaras fuente de todos los marcadores en el frame 1 de structure
+    %borro lo referente a la estructura frame
+    n_markers=get_info(structure, 'frame', frame(k), 'n_markers');%obtengo el numero de marcadores en el frame
+    n_index_k = length(index_x(1, colum));%numero de indices de index_x en el frame k
+    n_markers = n_markers - n_index_k; %actualizo el numero de marcadores en el frame k
+    structure = set_info(structure, 'frame', frame(k), 'n_markers', n_markers); 
+    %borro lo referente a la estructura path
+end
 
 %ordeno la estructura frame para dejar los marcadores vacios al final de la
 %lista de indices.
