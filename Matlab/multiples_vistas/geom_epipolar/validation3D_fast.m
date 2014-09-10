0bbb82dcb994e25758e9261e29440b4840ff1e78
mgrrwu <mgrrwu@gmail.com>
2014-09-10 02:18:09 -0300

    Se emepiza a optimizar las funciones de reconstrucción

--- Matlab/multiples_vistas/geom_epipolar/validation3D_fast.m
+++ Matlab/multiples_vistas/geom_epipolar/validation3D_fast.m
@@ -1,5 +1,5 @@
-function validation3D_fast(cam, n_cam1, n_cam2, n_frame, varargin)
-
+function [X, validation, d]=validation3D_fast(cam, n_cam1, n_cam2, n_frame, varargin)
+tic
 %gestiono las entradas
  cam1=cam(n_cam1);
  cam2=cam(n_cam2);
@@ -36,51 +36,35 @@ if isempty(location_replace) %si no se va a reponer los puntos que ya validaron 
     valid_points=varargin{aux};
 end
     
-
-%incializo las salidas
-n_cams = size(cam, 2); %numero total de camaras del experimento
-n_points = size(index_x1, 2);
-validation=zeros(n_cams-2, n_points);
-index_x3 = -ones(n_cams-2, n_points);
-if debug_on
-    dist = cell(1, n_points);
-    for  k=1:n_points
-        dist{k}=-ones(3, n_cams-2);
-    end
-else
-    dist = -ones(n_cams-2, n_points);
-end
+n_cams=size(cam, 2);
 
 
 %reconstruyo los puntos 3D X
 X = reconstruccion3D(cam1, cam2, n_frame, index_x1, index_x2);
-
-%obtengo matrices de proyeccion de cam1 y cam2 y puntos correspondientes
-P1 = get_info(cam1, 'projection_matrix'); %matrix de proyeccion de cam1
-P2 = get_info(cam2, 'projection_matrix'); %matrix de proyeccion de cam2
-x1 = get_info(cam1,'frame', n_frame, 'marker', index_x1, 'coord'); %devuelve las coordenadas de los marcadores en index_x1 del frame n_frame de cam1
-x2 = get_info(cam2,'frame', n_frame, 'marker', index_x2, 'coord'); %devuelve las coordenadas de los marcadores en index_x2 del frame n_frame de cam2
-
-
 %se efectua procedimiento para cam3, cam3 es toda camara distinta de cam1 y cam2
 
 vec_cam3 = find( ((1:n_cams)~=n_cam1)&((1:n_cams)~=n_cam2) )';%devuelve un vector con todas los numeros de camaras que no sean cam1 o cam2
 n_cam3 = length(vec_cam3);
-index = index_x1; %esta variable es utilizada para mantener los indices de los puntos para los cuales no se tiene una reconstruccion valida
-                       %Vector que indica cuyos elementos son los indices de los X que aun no fueron validados                        
 %Obtengo todos los x3 de las posibles camaras cam3
-x3 = cell(1,n_cam3);
+n_x3 = ones(1,n_cam3);
 for i=1:(n_cam3)
-    n_x3 = get_info(cam(i), 'frame', n_frame, 'n_markers'); %marcadores de la cam(i) en el frame n_frame
+    %n_x3 = get_info(cam(i), 'frame', n_frame, 'n_markers'); %marcadores de la cam(i) en el frame n_frame
     %devuelve las coordenadas de los marcadores del frame n_frame de cam(i)
     %junto con el numero de camara correspondiente en la tercer coordenada
-    x3{i}=get_info(cam(i),'frame', n_frame, 'marker', 'coord');  
-    x3{i} = [x3{i}(1:2, :); i*ones(1, n_x3) ];
-    
-end
- x3=cell2mat(x3);  
+    %x3{i}=get_info(cam(i),'frame', n_frame, 'marker', 'coord');  
+    %x3{i} = [x3{i}(1:2, :); i*ones(1, n_x3) ];
+    n_x3(i)= get_info(cam(i), 'frame', n_frame, 'n_markers'); %marcadores de la cam(i) en el frame n_frame
+end 
  
- x1 = x1*ones(1, n_frame*n_cam3)
- X = X*ones(1, n_frame*n_cam3)
-                       
+ %X = X*ones(1, sum(n_x3))
+ X3 =cell(1, n_cam3); %inicializo X3
+ 
+ for i=1:n_cam3 
+     index_x3 = 1:n_x3(i);%vector con los indices de los vectores x3 de la camara i     
+     X3{i}=reconstruccion3D(cam1, cam2, n_frame, index_x1*ones(1, n_x3(i)), index_x3);
+ end
+ X3 = cell2mat(X3);
+ d=pdist2(X', X3'); %distancia de todos los puntos X3 reconstruidos al punto X
+ validation = d>umbral;
+ toc
 end
