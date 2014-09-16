ebbaec3c8f40b9a09f131039ce3dfae8ce69c9b3
mgrrwu <mgrrwu@gmail.com>
2014-09-16 03:47:03 -0300

    Cambios mínimos para probar las versiones "_fast"

--- Matlab/multiples_vistas/geom_epipolar/main2.m
+++ Matlab/multiples_vistas/geom_epipolar/main2.m
@@ -1,5 +1,5 @@
 %este script es el prototipo del sistema completo permite integrar todas las partes
-clc
+
 %Con esto se pretende tener un tratamiento en paralelo de algunos ciclos for
 if matlabpool('size') == 0 % revisar si aun no se tiene algun matlabpool abierto   
     matlabpool open 
@@ -31,73 +31,90 @@ path_mat = [current_dir '/saved_vars']; %donde se guardan las estructuras .mat l
 %name = '13_segmentacion';
 name = '_clinicas'; 
 
-%% Segmentacion
-
-disp('__________________________________________________')
-disp('Se inicia el proceso de Segmentacion.')
-%list_XML = segmentacion(path_vid, type_vid, path_program, path_XML);
-%disp('La segmentacion a culminado con exito.')
-
-%%%%%%%%%%%%%%%%%%%% Esto se utiliza para empezar a partir de archivos xml en una cierta carpeta, luego debo hacer una funcion con esto
-list_XML=dir(fullfile(path_XML,'*.xml'));
-list_XML={list_XML.name}';
-
-index1 = strfind(list_XML{1}, '1.'); %index-1 indica donde termina el nombre y empiezan los numeros
-n_files = length(list_XML); % cantidad de archivos con extensi�n xml
-num_in_name = zeros(1, n_files);
-for k=1:n_files %la idea es pasar cada numero de un nombre a un vector y luego ordenarlo
-    index2 = strfind(list_XML{k}, '.') -1; %indice que indica hasta donde van los numeros finales
-    num_in_name(k)=str2double(list_XML{k}(index1-1:index2)); %guardo el numero de archivo
-end
-[~, sort_index] = sort(num_in_name);
-list_XML = list_XML(sort_index);
-%%%%%%%%%%%%%%%%%%5
-
-
-%% Pasaje a estructuras .mat
-tic
-disp('__________________________________________________')
-disp('Se inicia el pasaje de archivos .xml a estructuras .mat.')
-
-n_cams = length(list_XML); %numero de camaras importadas
-n_markers = 3*length(names); %nro de marcadores a detectar
-
-%cargo los archivos xml provenientes de la segmentacion asi como los datos de las camaras Blender
-cam_segmentacion = markersXML2mat(names, path_XML, list_XML);
-
-
-%%%%%%%%%%%%ESTO SE AGREGA POR AHORA PUES SE DEBE ARREGLAR LA FUNCION DE INICIALIZACION DE ESTRUCTURAS SIN BLENDER
-if strcmp(name, '_clinicas') %en este caso debo trabajar solo algunas camaras
-    cam_segmentacion=cam_segmentacion(1:n_cams)
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-n_frames = get_info(cam_segmentacion(1), 'n_frames');%obtengo el numero de frames de la camara 1. 
-                                                    %(supuestamente todas las camaras deberian tener igual nro de frames). Si esto no ocurre
-                                                    %deberia haber algun warning en la terminal matlab producido por markersXML2mat.
-
-%inicializo una estructura skeleton para completar a medida que se trabaje con los datos de cam_segmentacion
-[~ , skeleton_segmentacion]=init_structs(n_markers, n_frames, names);
-skeleton_segmentacion = set_info(skeleton_segmentacion, 'name_bvh', name_bvh); %setea string nombre de la estructura
-skeleton_segmentacion=set_info(skeleton_segmentacion, 'name', ['skeleton' name])
-
-%guardo la informacion
-save([path_mat '/cam' name ], 'cam_segmentacion')
-str = sprintf('Se a guardado el resultado de la segmentacion de las camaras en %s/cam%s', path_mat, name);
-disp(str)
-save([path_mat '/skeleton' name ], 'skeleton_segmentacion')
-disp('El pasaje a estructuras .mat a culminado.')
-toc
+% %% Segmentacion
+% 
+% disp('__________________________________________________')
+% disp('Se inicia el proceso de Segmentacion.')
+% %list_XML = segmentacion(path_vid, type_vid, path_program, path_XML);
+% %disp('La segmentacion a culminado con exito.')
+% 
+% %%%%%%%%%%%%%%%%%%%% Esto se utiliza para empezar a partir de archivos xml en una cierta carpeta, luego debo hacer una funcion con esto
+% list_XML=dir(fullfile(path_XML,'*.xml'));
+% list_XML={list_XML.name}';
+% 
+% index1 = strfind(list_XML{1}, '1.'); %index-1 indica donde termina el nombre y empiezan los numeros
+% n_files = length(list_XML); % cantidad de archivos con extensi�n xml
+% num_in_name = zeros(1, n_files);
+% for k=1:n_files %la idea es pasar cada numero de un nombre a un vector y luego ordenarlo
+%     index2 = strfind(list_XML{k}, '.') -1; %indice que indica hasta donde van los numeros finales
+%     num_in_name(k)=str2double(list_XML{k}(index1-1:index2)); %guardo el numero de archivo
+% end
+% [~, sort_index] = sort(num_in_name);
+% list_XML = list_XML(sort_index);
+% %%%%%%%%%%%%%%%%%%5
+% 
+% 
+% %% Pasaje a estructuras .mat
+% tic
+% disp('__________________________________________________')
+% disp('Se inicia el pasaje de archivos .xml a estructuras .mat.')
+% 
+% n_cams = length(list_XML); %numero de camaras importadas
+% n_markers = 3*length(names); %nro de marcadores a detectar
+% 
+% %cargo los archivos xml provenientes de la segmentacion asi como los datos de las camaras Blender
+% cam_segmentacion = markersXML2mat(names, path_XML, list_XML);
+% 
+% 
+% %%%%%%%%%%%%ESTO SE AGREGA POR AHORA PUES SE DEBE ARREGLAR LA FUNCION DE INICIALIZACION DE ESTRUCTURAS SIN BLENDER
+% if strcmp(name, '_clinicas') %en este caso debo trabajar solo algunas camaras
+%     cam_segmentacion=cam_segmentacion(1:n_cams)
+% end
+% %%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% n_frames = get_info(cam_segmentacion(1), 'n_frames');%obtengo el numero de frames de la camara 1. 
+%                                                     %(supuestamente todas las camaras deberian tener igual nro de frames). Si esto no ocurre
+%                                                     %deberia haber algun warning en la terminal matlab producido por markersXML2mat.
+% 
+% %inicializo una estructura skeleton para completar a medida que se trabaje con los datos de cam_segmentacion
+% [~ , skeleton_segmentacion]=init_structs(n_markers, n_frames, names);
+% skeleton_segmentacion = set_info(skeleton_segmentacion, 'name_bvh', name_bvh); %setea string nombre de la estructura
+% skeleton_segmentacion=set_info(skeleton_segmentacion, 'name', ['skeleton' name])
+% 
+% %guardo la informacion
+% save([path_mat '/cam' name ], 'cam_segmentacion')
+% str = sprintf('Se a guardado el resultado de la segmentacion de las camaras en %s/cam%s', path_mat, name);
+% disp(str)
+% save([path_mat '/skeleton' name ], 'skeleton_segmentacion')
+% disp('El pasaje a estructuras .mat a culminado.')
+% toc
 %% Reconstruccion
-tic
+
+n_cams = 17;
+n_frames=300;
+n_markers = 14;
+
+
+
 vec_cams = 1:n_cams;
 umbral = 0.05;
 Xrec = cell(1, 300);
 disp('__________________________________________________')
 disp('Se inicia el proceso de Reconstruccion')
+
+%obtengo datos que no dependen del frame con el que se este trabajando
+P = cell(1, n_cams);
+C = cell(1, n_cams);
+
+for i=vec_cams    
+    P{i} = get_info(cam_segmentacion(i), 'projection_matrix'); %matrix de proyeccion de la camara   
+    C{i} = null(P{i}); %punto centro de la camara, o vector director perpendicular a la retina
+end
+
 parfor frame=1:n_frames %hacer para cada frame (Se efectua en paralelo)
     %efectuo la reconstruccion de un frame    
-    Xrec{frame} = reconstruccion1frame(cam_segmentacion, vec_cams, frame, umbral, n_markers);
+    %Xrec{frame} = reconstruccion1frame(cam_segmentacion, vec_cams, frame, umbral, n_markers);
+    Xrec{frame} = reconstruccion1frame(cam_segmentacion, vec_cams, P, C, frame, umbral, n_markers);
     %genero aviso     
     str=sprintf('Se ha reconstruido el frame %d', frame);
     disp(str)    
@@ -111,7 +128,7 @@ for  frame=1:n_frames %hacer para cada frame (Se efectua secuencialmente)
     skeleton_segmentacion = set_info(skeleton_segmentacion, 'frame', frame, 'marker', 'coord', Xrec{frame}); %ingreso los marcadores en el frame correspondiente de estructura_salida
     skeleton_segmentacion = set_info(skeleton_segmentacion, 'frame', frame, 'n_markers', n_Xrec);% actualizo el numero de frame correspondiente
 end
-toc
+
 save([path_mat '/skeleton' name ], 'skeleton_segmentacion')
 %% Tracking
 
