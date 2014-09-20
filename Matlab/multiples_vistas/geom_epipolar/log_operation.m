4324f0fe6b787e61917da9fe305d6b4d1cd5f549
mgrrwu <mgrrwu@gmail.com>
2014-09-19 21:10:08 -0300

    Función que permite tener una idea de por donde se esta dentro de un ciclo parfor

---
+++ Matlab/multiples_vistas/geom_epipolar/log_operation.m
@@ -0,0 +1,16 @@
+function contador = log_operation(str)
+%se utiliza esta funcion para contar cuantas veces se entra a un ciclo
+%parfor
+
+global contador
+if strcmp(str, 'reset')
+    contador = 0;
+else if isempty(contador)
+        contador = 0;
+    end
+    matlabpool size;
+    n = ans; %encuentro cuantos procesadores se estan usando
+    contador = contador + n;
+end
+
+end
