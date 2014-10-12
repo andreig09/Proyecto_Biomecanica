75c77209466573f30416e45e14fd2dbb7c6f9e83
gonzalito <gonzpk@gmail.com>
2014-10-11 22:29:00 -0200

    Test Corredor
    testeo de tracking de corredor

---
+++ SISTEMA/Tracking/clean_tracking.m
@@ -0,0 +1,15 @@
+function X_out = clean_tracking(X_out)
+%% Limpieza de puntos no trackeados
+X_out = X_out(:,X_out(7,:)~=0);
+
+%% Limpieza de trayectorias truncas
+
+total_frames = max(X_out(4,:))-min(X_out(4,:));
+
+for n_path=1:max(X_out(5,:))
+    if size(X_out(:,X_out(5,:)==n_path),2)<0.9*total_frames
+        X_out = X_out(:,X_out(5,:)~=n_path);
+    end
+end
+
+end
