1440fcd1f9833b28381d9484a1ea486ae8a7e282
gonzalito <gonzpk@gmail.com>
2014-12-03 18:37:53 -0200

    Tracking Gonzalo
    Actualizacion a las versiones que estaba usando para probar

--- SISTEMA/test_reconstruccion.m
+++ SISTEMA/test_reconstruccion.m
@@ -3,13 +3,14 @@ close all
 clc
 add_paths()
 
-load 'C:\Proyecto\Proyecto_Biomecanica\Archivos_mat\CMU_8_07_hack\1600_600-100-200\Reconstruccion\skeleton.mat'
+frame_ini = 10;
+
+load 'C:\Proyecto\PB_2014_11_27\Archivos_mat\CMU_8_07_hack\1600_600-100-200\Reconstruccion\skeleton.mat'
 
 skeleton_reconstruccion = skeleton_rec;
 
 %return;
-
-load 'C:\Proyecto\Proyecto_Biomecanica\Archivos_mat\CMU_8_07_hack\1600_600-100-200\Ground_Truth\Reconstruccion\skeleton.mat';
+load 'C:\Proyecto\PB_2014_11_27\Archivos_mat\CMU_8_07_hack\1600_600-100-200\Ground_Truth\Reconstruccion\skeleton.mat';
 
 skeleton_ground = skeleton_rec;
 
@@ -19,10 +20,14 @@ clear skeleton_rec;
 
 Xi=[];Yi=[];
 
-for frame=10:get_info(skeleton_reconstruccion,'n_frames')
+for frame=frame_ini:get_info(skeleton_reconstruccion,'n_frames')
     xi = get_info(skeleton_reconstruccion,'frame', frame, 'marker', 'coord');
     
-    Xi=[Xi,[xi;frame*ones(1,size(xi,2))]];
+    Xi=[Xi,...
+        [xi;...
+        frame*ones(1,size(xi,2));...
+        1:size(xi,2)]...
+        ];
     
     X_aux1 = [Xi(:,Xi(4,:)==frame)];
     X_aux2 = sortrows(unique(X_aux1','rows'),4)';
@@ -40,18 +45,26 @@ for frame=10:get_info(skeleton_reconstruccion,'n_frames')
     end
 end
 
-for frame=1:get_info(skeleton_ground,'n_frames')
+for frame=frame_ini:get_info(skeleton_ground,'n_frames')
     yi = get_info(skeleton_ground,'frame', frame, 'marker', 'coord');
-    Yi=[Yi,[yi;frame*ones(1,size(yi,2))]];
+    Yi=[Yi,...
+        [yi;...
+        frame*ones(1,size(yi,2));...
+        1:size(yi,2)]];
 end
 
 %% Testeo de Error de Reconsturccion
 
-[a,b,c,d]=rmse_segmentacion_ground(Xi,Yi);
+[rmse,rmse_i,~,~,~,rmse_i_t]=rmse_segmentacion_ground(Xi,Yi);
 
-disp([ 'Promedio = ' num2str(a*100) ' cm' ])
+porcentaje = 99;
 
-disp([ '99% = ' num2str(prctile(b(1,:),99)*100) ' cm' ])
+disp([ 'Promedio = ' num2str(mean(rmse_i_t(1,:))*100) ' cm' ])
+
+disp([ sprintf('%d%%',porcentaje) ' = ' num2str(prctile(rmse_i(1,:),porcentaje)*100) ' cm' ])
 
 
-disp([ 'Media = ' num2str(median(b(1,:))*100) ' cm' ])
+disp([ 'Media = ' num2str(median(rmse_i(1,:))*100) ' cm' ])
+
+disp([ num2str(max(rmse_i(3,:))) ' markers' ]);
+%animar_tracking(Xi(:,Xi(1,:)<=max(Yi(1,:))&Xi(2,:)<=max(Yi(2,:))&Xi(3,:)<=max(Yi(3,:))&Xi(1,:)>=min(Yi(1,:))&Xi(2,:)>=min(Yi(2,:))&Xi(3,:)>=min(Yi(3,:))))
