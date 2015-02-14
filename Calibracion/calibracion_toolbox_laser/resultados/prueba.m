51bdffaca9c44a9728e4335e554a6bb7fca3a920
mgrrwu <mgrrwu@gmail.com>
2015-02-14 17:41:37 -0200

    Se ingresa una carpeta para trabajar con Calibracion.

---
+++ Calibracion/calibracion_toolbox_laser/resultados/prueba.m
@@ -0,0 +1,56 @@
+clc
+
+
+%se debe tener cargada un ground truth de cam.mat para que estoy funcione
+%además se debe estar en la carpeta que contiene los archivos 'camera%d.Pmat.cal'
+
+ncam= 3;%retina que se utiliza para comparar proyecciones 
+%inicializo variables
+P1=cell(1, 17);%va a contener las matrices de proyeccion obtenidas a partir de calibracion con puntos luminicos
+P2 =cell(1, 17);%va a contener las matrices de proyeccion obtenidas a partir de ground truth
+C1=ones(4, 17);%va a contener los centros de las camaras a partir de P1
+C2=ones(4, 17);%va a contener los centros de las camaras a partir de P2
+for i=1:17
+    
+    P1{i}=load(['camera' num2str(i) '.Pmat.cal']);
+    C1(:,i)=null(P1{i});
+   
+    
+    P2{i}=str2num(cam_seg{i}.Attributes.projection_matrix);
+    C2(:,i)=null((P2{i}));
+   
+end
+
+%normalizo los puntos 3D
+C1=homog_norm(C1)
+C2=homog_norm(C2)
+
+
+
+
+%C1=T.C2 ==>T=C1/C2
+T=C1/C2%T es la matriz que lleva de los C2 a los C1.
+
+%Por ahora esto no da igual pues se debe corregir el efecto de inverción de
+%ejes
+P1{ncam}*T-(P2{ncam});%FALTA SOLUCIONAR
+
+
+%Encuentro los puntos 3D centros de las camaras proyectados en la retina
+%ncam
+
+%proyecto los C1
+x1=P1{3}*C1;
+x1=homog_norm(x1);
+%proyecto los C2
+x2=P2{3}*C2;
+x2=homog_norm(x2);
+
+%modifico el origen en el plano de retina que se utiliza para escribir los x2 para que coincida con el origen
+%de las coordenadas en que fueron escritos los x1
+resolution=str2num(cam_seg{i}.Attributes.resolution);
+x2(2,:)=-x2(2,:)+resolution(2)+1;
+
+
+%Comparo ambas coordenadas
+diferencia=x1-x2
