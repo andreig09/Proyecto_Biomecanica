01f860c76a2ddcf70e879da1595e57d32aca1dc0
gonzalito <gonzpk@gmail.com>
2015-02-18 20:22:07 -0200

    Acttualizaciones Test Tracking

--- SISTEMA/img_angulo_cuerpo.m
+++ SISTEMA/img_angulo_cuerpo.m
@@ -1,13 +1,13 @@
 clc
 close all
 
-skeleton = [13,14;14,10;10,7;7,1;1,15;10,12;12,7;12,5;5,4;4,2;2,11;5,8;8,9;9,6];
+skeleton = [11,12;12,6;6,13;13,7;7,1;6,8;8,13;8,3;3,9;9,4;4,2;3,15;15,5;5,10];
 
 %% Primer marcador es la articulacion
 
-marker_i = 14;marker_j = 13;marker_k = 10;
+marker_i = 12;marker_j = 6;marker_k = 11;
 
-frame_a = 27;frame_b = 51;frame_c = 76;
+frame_a = 10;frame_b = 60;frame_c = 110;
 
 angulo_ijk = animar_angulo(X_out,marker_i,marker_j,marker_k);
 
@@ -20,6 +20,7 @@ skeleton_bx = [];skeleton_by = [];skeleton_bz = [];
 skeleton_cx = [];skeleton_cy = [];skeleton_cz = [];
 
 for bone=1:size(skeleton,1)
+    
     skeleton_ax = [skeleton_ax;X_out(1,X_out(5,:)==skeleton(bone,1)&X_out(4,:)==frame_a),X_out(1,X_out(5,:)==skeleton(bone,2)&X_out(4,:)==frame_a)];
     skeleton_ay = [skeleton_ay;X_out(2,X_out(5,:)==skeleton(bone,1)&X_out(4,:)==frame_a),X_out(2,X_out(5,:)==skeleton(bone,2)&X_out(4,:)==frame_a)];
     skeleton_az = [skeleton_az;X_out(3,X_out(5,:)==skeleton(bone,1)&X_out(4,:)==frame_a),X_out(3,X_out(5,:)==skeleton(bone,2)&X_out(4,:)==frame_a)];
@@ -78,9 +79,9 @@ figure
 plot3(X_out(1,X_out(5,:)==marker_i),X_out(2,X_out(5,:)==marker_i),X_out(3,X_out(5,:)==marker_i),'b.-',...
     X_out(1,X_out(5,:)==marker_j),X_out(2,X_out(5,:)==marker_j),X_out(3,X_out(5,:)==marker_j),'r.-',...
     X_out(1,X_out(5,:)==marker_k),X_out(2,X_out(5,:)==marker_k),X_out(3,X_out(5,:)==marker_k),'g.-',...
-    skeleton_ax',skeleton_ay',skeleton_az','bo-',...
-    skeleton_bx',skeleton_by',skeleton_bz','bo-',...
-    skeleton_cx',skeleton_cy',skeleton_cz','bo-');
+    skeleton_ax',skeleton_ay',skeleton_az','ko-',...
+    skeleton_bx',skeleton_by',skeleton_bz','ko-',...
+    skeleton_cx',skeleton_cy',skeleton_cz','ko-');
 
 labels = cellstr(num2str([L_a,L_b,L_c]'));
 text([X_a(1,:),X_b(1,:),X_c(1,:)],...
--- SISTEMA/test_corredor.m
+++ SISTEMA/test_corredor.m
@@ -1,8 +1,6 @@
 clc
 close all
 
-%{
-
 clear all
 
 add_paths()
@@ -11,7 +9,7 @@ frame_ini = 10;
 
 %%
 
-load 'C:\Proyecto\Base_20150202\Base_de_datos\Sujeto_CMU_09\09_12\Ground_Truth\1600_600-100-100\Reconstruccion\skeleton.mat';
+load 'C:\Proyecto\Base_de_datos\Sujeto_CMU_08\08_07\Ground_Truth\1600_600-100-200\Reconstruccion\skeleton.mat';
 
 skeleton_ground = skeleton_rec;
 n_frames = get_info(skeleton_rec,'n_frames');
@@ -27,7 +25,7 @@ end
 
 %%
 
-load 'C:\Proyecto\Base_20150202\Base_de_datos\Sujeto_CMU_09\09_12\Datos_Procesados\1600_600-100-100\Reconstruccion\skeleton.mat'
+load 'C:\Proyecto\Base_de_datos\Sujeto_CMU_08\08_07\Datos_Procesados\1600_600-100-200\Reconstruccion\skeleton.mat'
 
 n_frames = get_info(skeleton_rec,'n_frames');
 %n_frames = 30;
@@ -40,7 +38,7 @@ Xi = [];
 
 for frame=frame_ini:n_frames
     xi = get_info(skeleton_rec,'frame', frame, 'marker', 'coord');
-    Xi=[Xi,[xi;frame*ones(1,size(xi,2))]];
+    Xi=[Xi,[xi;frame*ones(1,size(xi,2));1:size(xi,2)]];
 end
 
 %%
@@ -48,16 +46,19 @@ end
 [X_out,datos_aux]=make_tracking(Xi,Inf);
 X_out = clean_tracking(X_out);
 
+marker_ground = 10;
+
 %% FILTRADO GLOBAL
 
+[X_out,datos_aux]=make_tracking(Xi,prctile(X_out(7,:),97));
 %[X_out,datos_aux]=make_tracking(Xi,prctile(X_out(7,:),99));
-%X_out = clean_tracking(X_out);
+X_out = clean_tracking(X_out);
 
 
 %% FILTRADO INDIVIDUAL
 
 %[~,thr] = filter_tracking(X_out);
-[X_out,thr] = filter_tracking(X_out);
+%[X_out,thr] = filter_tracking(X_out);
 
 %%
 
@@ -66,10 +67,6 @@ X_out = clean_tracking(X_out);
 
 %[X_out,datos]=make_tracking(Xi,prctile(X_out(6,:),99.78));
 %X_out = clean_tracking(X_out);
-
-%}
-
-marker_ground = 2;
 
 %animar_tracking(X_out)
 
@@ -129,6 +126,9 @@ marker_tracking = error_marker(error_marker(:,2)==marker_ground,1);
 
 X_1=X_out(:,X_out(5,:)==marker_tracking);
 X_2=Yi(:,Yi(5,:)==marker_ground&Yi(4,:)<=max(X_out(4,:)));
+
+X_1 = X_1(:,1:min([length(X_1),length(X_2)])); X_2 = X_2(:,1:min([length(X_1),length(X_2)]));
+
 figure
 plot(X_out(4,X_out(5,:)==marker_tracking),100*(sum((X_1(1:3,:)-X_2(1:3,:)).^2)).^0.5,'.-')
 title([' Error - Tracking: ' num2str(marker_tracking) ' ,Ground: ' num2str(marker_ground)])
@@ -179,18 +179,20 @@ for n_path=1:size(n_paths,2)
         plot(X_path(4,2:size(X_path,2)),velocidad,'b.-',...
             [min(X_path(4,3:size(X_path,2))),max(X_path(4,3:size(X_path,2)))],median(prctile(velocidad,90:0.1:100))*[1,1],'r--')
         title(['Marker ' num2str(n_paths(n_path)) ' - Velocidad']);
+        xlabel('Frame');ylabel('metro/frame');
         
         subplot(3,1,2)
         aceleracion = sum((-X_path(1:3,3:size(X_path,2))+2*X_path(1:3,2:size(X_path,2)-1)-X_path(1:3,1:size(X_path,2)-2)).^2).^(1/2);
         plot(X_path(4,3:size(X_path,2)),aceleracion,'b.-',...
-            [min(X_path(4,3:size(X_path,2))),max(X_path(4,3:size(X_path,2)))],thr(thr(:,1)==path,2)*[1,1],'r--')
+            [min(X_path(4,3:size(X_path,2))),max(X_path(4,3:size(X_path,2)))],median(prctile(aceleracion,90:0.1:100))*[1,1],'r--')
         title(['Marker ' num2str(n_paths(n_path)) ' - Aceleracion']);
+        xlabel('Frame');ylabel('metro/frame^2');
         subplot(3,1,3)
         v_aceleracion = sum((-X_path(1:3,4:size(X_path,2))+3*X_path(1:3,3:size(X_path,2)-1)-3*X_path(1:3,2:size(X_path,2)-2)+X_path(1:3,1:size(X_path,2)-3)).^2).^(1/2);
         plot(X_path(4,4:size(X_path,2)),v_aceleracion,'b.-',...
             [min(X_path(4,3:size(X_path,2))),max(X_path(4,3:size(X_path,2)))],median(prctile(v_aceleracion,90:0.1:100))*[1,1],'r--')
         title(['Marker ' num2str(n_paths(n_path)) ' - Var.Aceleracion']);
-        
+        xlabel('Frame');ylabel('metro/frame^3');
         if n_path<length(n_paths)
             pause
         end
